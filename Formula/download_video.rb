a4542967e2b127829adb72240ac5f8d027f70f90
daite <blueskykind02@yahoo.co.jp>
2024-08-25 22:05:37 +0900

    updated

--- Formula/download_video.rb
+++ Formula/download_video.rb
@@ -5,12 +5,12 @@ class DownloadVideo < Formula
     sha256 "b1a4db5049943834ba7ecc30aba93928ca111d894eb8611ee803ef63a9d34a77"  # Calculate the SHA256 checksum of the script file
   
     def install
-        bin.install "./code/download_video.sh" => "download_video"
-        chmod 0755, bin/"download_video"
+        bin.install "./code/download_video.sh" => "dv"
+        chmod 0755, bin/"dv"
       end
     
       test do
         # Basic test to ensure the script runs without errors
-        assert_match "Usage:", shell_output("#{bin}/download_video --help 2>&1")
+        assert_match "Usage:", shell_output("#{bin}/dv --help 2>&1")
       end
 end
